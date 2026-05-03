49ff83ce8ecf3e8725d6d21bdd18a2575f6c355c
Ahmed <alshukairi1234@gmail.com>
2026-05-03 15:30:10 +0400

    added basic.sql file

---
+++ basic.sql
@@ -0,0 +1,22 @@
+SELECT Flight_number, Status, Departure_datetime 
+FROM Flight 
+ORDER BY Departure_datetime ASC;
+
+SELECT * FROM Passenger 
+ORDER BY Full_name;
+
+SELECT Model, Capacity 
+FROM Aircraft 
+ORDER BY Capacity DESC;
+
+SELECT DISTINCT Class 
+FROM Booking;
+
+SELECT * FROM Flight 
+WHERE Status IN ('Delayed', 'Cancelled');
+
+SELECT * FROM Passenger 
+WHERE Nationality = 'Omani';
+
+SELECT * FROM Airport 
+ORDER BY Country;
